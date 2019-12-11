a9623d1b5c3daba1463c834e93a4f6aa5356fa6b
lauren brett <laurencareybrett@gmail.com>
2019-12-11 15:05:14 +0000

    extentions

---
+++ students_controller.rb
@@ -0,0 +1,53 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/student')
+require_relative('models/house')
+also_reload('./models/*')
+
+
+
+# index
+get '/students' do
+  @students = Student.all()
+  erb(:index)
+end
+
+# new
+get '/students/new' do
+  @houses = House.all()
+  erb(:new)
+end
+
+# show
+get '/students/:id' do
+  @student = Student.find( params[:id].to_i)
+  erb(:student)
+end
+
+
+# create
+post '/students' do
+  @student = Student.new(params)
+  @student.save()
+  erb(:create)
+end
+
+# edit
+get '/students/:id/edit' do
+  @student = Student.find(params[:id].to_i)
+  @houses = House.all()
+  erb(:edit)
+end
+
+# update
+post '/students/:id' do
+  Student.new(params).update
+  redirect to '/students'
+end
+
+# destroy
+post '/students/:id/delete' do
+  @student = Student.find(params[:id].to_i)
+  @student.delete
+  redirect to '/students'
+end
